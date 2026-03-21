89aca0d095e79469b44d692c2765e4835a93f3f3
zhanel <s.aizhan.b@mail.ru>
2026-03-21 12:53:31 +0500

    new

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import ("fmt"; "net/http"; "html/template")
 
 type User struct{
-	name string 
-	age uint16
-	money int16
-	avg_grades, happiness float64
+	Name string 
+	Age uint16
+	Money int16
+	Avg_grades, Happiness float64
 }
 
 func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money )
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money )
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request){
-	bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8 }
+	bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 4.2, Happiness: 0.8 }
 	// bob.setNewName("Zhan")
 	// fmt.Fprintf(w, bob.getAllInfo())
 	tmpl, _ := template.ParseFiles("templates/home_page.html")
