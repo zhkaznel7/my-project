cc2b721ead16210ab844855d8364c170babadd30
zhanel <s.aizhan.b@mail.ru>
2026-03-25 20:28:09 +0500

    save_article

--- main.go
+++ main.go
@@ -10,8 +10,26 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 } 
 
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+} 
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/save_article.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "save_article", nil)
+} 
+
 func handleFunc() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create/", create)
+	http.HandleFunc("/save_article/", save_article)
 	http.ListenAndServe(":8080", nil)
 } 
 
