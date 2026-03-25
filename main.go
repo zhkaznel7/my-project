131d43a3349bc6f682f51499d3d2952b15f707de
zhanel <s.aizhan.b@mail.ru>
2026-03-25 21:44:59 +0500

    new

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main 
 
-import ("fmt"; "net/http"; "html/template")
+import ("fmt"; "net/http"; "html/template"; "database/sql";
+	_ "github.com/go-sql-driver/mysql")
+
+type Article struct {
+	Id uint16
+	Title, Anons, FullText string
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT * From `articles` ")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Close() // Ресурсты жабуды ұмытпаймыз
+
+	for res.Next() {
+		var post Article
+		// Scan арқылы базадағы деректі айнымалыға меншіктейміз
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText )
+		if err != nil {
+			panic(err)
+		}
+		// Айнымалылар fmt.Sprintf-тің тырнақшасынан кейін үтірмен жазылады
+		fmt.Println(fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id ))
+	}
+
 	t.ExecuteTemplate(w, "index", nil)
 } 
 
@@ -19,12 +49,31 @@ func create(w http.ResponseWriter, r *http.Request) {
 } 
 
 func save_article(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/save_article.html", "templates/header.html", "templates/footer.html")
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == ""{
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		panic(err)
 	}
-	t.ExecuteTemplate(w, "save_article", nil)
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+	if err != nil {
+		panic(err)
+	}
+	defer res.Close() // Ресурсты жабуды ұмытпаймыз
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+
+
 } 
+
 
 func handleFunc() {
 	http.HandleFunc("/", index)
